54ad98132047d34a04d35936d9278c841625267b
1100110 <jude@1100110>
2012-06-04 21:38:59 -0500

    Yay! A makefile I can use! Still needs debugging options of course

--- deimos/examples/makeExample.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-CC=`which gdc`
-LIBS=`pkg-config --cflags --libs elementary`
-CFLAGS="-Os -pipe -march=native -g"
-OUT=`echo $1 | cut -d "." -f 1`
-
-echo "$CC $CFLAGS $1 -o $OUT $LIBS"
-$CC $CFLAGS $1 -o $OUT $LIBS
--- deimos/examples/makeexample.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-CC=`which gcc`
-LIBS=`pkg-config --cflags --libs elementary`
-CFLAGS="-Os -pipe -march=native -g"
-OUT=`echo $1 | cut -d "." -f 1`
-$CC $CFLAGS $1 -o $OUT $LIBS
